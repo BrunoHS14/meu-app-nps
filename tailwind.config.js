7ca000d66120db2d02b50fd9ab9752fab85ce897
Bruno <bruno999893@gmail.com>
2026-03-28 18:27:20 -0300

    adiciona cores ECOA no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        'ecoa-blue': '#3B82F6',
+        'ecoa-purple': '#8B5CF6',
+        'ecoa-violet': '#7C3AED',
+        'ecoa-dark': '#0F172A',
+      }
+    }
+  },
   plugins: [],
 }
